375e4f76de1fa92a5bbc9e0d85d3bbe45b70e909
aureahariki <aureahariki@gmail.com>
2018-06-09 23:54:13 -0300

    Add files via upload

---
+++ src/components/AppBackground.vue
@@ -0,0 +1,16 @@
+<template>
+  <body>
+    <hr>
+      <div class="background">
+      </div>
+  </body>
+</template>
+
+<script>
+</script>
+
+<style>
+.background {
+  background-color: #e6ffff
+}
+</style>
---
+++ src/components/HomepageHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <hr>
+      <div class="header-menu">
+        <div class="menu-buttons">
+          <b-button><b-img src="menu.png"></b-img></b-button>
+          <b-button><b-img src="menu.png"></b-img></b-button>
+          <b-button><b-img src="menu.png"></b-img></b-button>
+          <b-button><b-img src="menu.png"></b-img></b-button>
+        </div>
+      </div>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style>
+.header-menu {
+  margin-top: 0;
+  height: 3em;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  background-color: #2286c3
+}
+.background {
+  background-color: #e6ffff
+}
+</style>
