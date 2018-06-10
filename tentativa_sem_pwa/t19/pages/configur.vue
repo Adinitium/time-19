86b9f355e4a4b0d955dd50c14f1ca2af8dda13c3
aureahariki <aureahariki@gmail.com>
2018-06-10 09:43:13 -0300

    mais paginasq

---
+++ tentativa_sem_pwa/t19/pages/configur.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="background">
+        <homepage-header></homepage-header>
+<h1>Configurações</h1>
+<ul>
+<li>Alterar configurações de privacidade</li>
+<li>Alterar senha</li>
+<li>Alterar e-mail</li>
+</ul>
+</div>
+</template>
+
+<script>
+import HomepageHeader from '../components/HomepageHeader.vue'
+
+export default {
+  components: {
+    HomepageHeader
+  }
+}
+</script>
---
+++ tentativa_sem_pwa/t19/pages/recomendados.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="background">
+        <homepage-header></homepage-header>
+        <div class="date">
+                Sábado 9 de junho:
+        </div>
+        <div class="boxed">
+            <div class="hora">
+                10:00am
+            </div> 
+            <img :src="require('@/assets/ime.jpg')" />
+            <div class="title">           
+                Aula de Aprendizado de Máquina
+            </div> 
+            <div class="descripcao">
+                Prof: Nina <br>
+                Sala: s. 144       
+            </div> 
+            <div class="tags">
+                #aula
+            </div> 
+        </div>
+        <div class="boxed">
+            <div class="hora">
+                6:00pm
+            </div> 
+            <img :src="require('@/assets/junime.jpg')" />
+            <div class="title">           
+                JunIME
+            </div> 
+            <div class="descripcao">
+                É mais uma JUNIME chegando nessa quadrilha demais de boa! E como sempre, teremos aquelas famosas delícias para esquentar esse arraiar sô! Vai ter:        
+            </div> 
+            <div class="tags">
+                #tecnologia #palestra académica
+            </div> 
+        </div>
+        <div class="boxed">
+                <div class="hora">
+                    5:00pm
+                </div> 
+                <img :src="require('@/assets/bienio.jpg')" />
+                <div class="title">           
+                    Aula de desenvolvimento de solucoes medico hospitarais
+                </div> 
+                <div class="descripcao">
+                        Prof:  <br>
+                        Sala: PRO s.10       
+                </div> 
+                <div class="tags">
+                    #aula
+                </div> 
+            </div>
+</div>
+</template>
+
+<script>
+import HomepageHeader from '../components/HomepageHeader.vue'
+
+export default {
+  components: {
+    HomepageHeader
+  }
+}
+</script>
